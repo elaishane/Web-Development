6babe338395dc01fd8455cb603e8e2b658f40179
Yash Adulkar <yash151099@gmail.com>
2020-11-28 13:09:42 +0530

    Drum Kit Completed Project

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,65 @@
+var numberOfDrums = document.querySelectorAll(".drum");
+
+for (var index = 0; index < numberOfDrums.length; index++) {
+    numberOfDrums[index].addEventListener("click", function (event) {
+        var buttonHtml = this.innerHTML;
+        makeSound(buttonHtml);
+        buttonAnimation(buttonHtml);
+    });
+}
+
+// Detecting Keyboard Press
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+
+});
+
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        default:
+            alert(key);
+            break;
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(() => {
+        activeButton.classList.remove("pressed");
+    },
+        100);
+
+}
+
+
